91b51bce35dc4d64539fdefecf28d3806870f831
Ahmed <ahmed.max7586@gmail.com>
2019-07-19 16:20:19 -0400

    added updateitme fn

--- scripts/api.js
+++ scripts/api.js
@@ -23,6 +23,15 @@ const api = (function(){
       body: newItem});
   }
 
+  function updateItem(id, updateData) {
+    return fetch(`${BASE_URL}/items/${id}`, {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(updateData)
+    });
+  }
+
+
   return {
     getItems,
     createItem,
