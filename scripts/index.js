13a55d7e6683af7cb550e7b38a79fc4625d5fb4f
m-cal <iengineersoftware@gmail.com>
2019-07-19 14:12:33 -0400

    hello world

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+'use strict';
+/* global shoppingList, store, Item */
+// eslint-disable-next-line no-unused-vars
+$(document).ready(function() {
+  shoppingList.bindEventListeners();
+  shoppingList.render();
+});
+
+store.items.push(Item.create('apples'));
